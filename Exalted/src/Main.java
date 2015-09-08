8e252c086d2b0cc8d65e443aed662a4a0fe3c3ba
Kim <kokobean22@gmail.com>
2015-09-08 02:11:46 -0400

    Custom Chars and Charms!
    Create Custom Characters and Charms?

--- Exalted/src/Main.java
+++ Exalted/src/Main.java
@@ -5,5 +5,14 @@ public class Main
 	{
 		Exalt tom = new Exalt();
 		tom.printChar();
+		System.out.println("-----------------------------------------------------------------------------------------------------------------------");
+		System.out.println();
+		System.out.println();
+		int[] maAb = {3,4,5,4,4,2,0,0,4,0,5,4,4,3,4,2,5,2,1,4,5,5,5,2,0,3,5,0,3,6};
+		Exalt marian = new Exalt("Marian", "Solar", "Night", 4, 5, 5, 5, 4, 6, 3, 4, 5, maAb, 9, 6, 2, 5, 3, 3, "Red Rage of Compassion");
+		marian.printChar();
+		
+		Charm test = new Charm();
+		test.printCharm();
 	}
 }
